766be646cd7298d098babc4f5f8533be43a8f2b2
farhankdi <43706147+farieas@users.noreply.github.com>
2025-11-18 23:44:38 +0530

    Update idx-template.nix

--- flutter-create/idx-template.nix
+++ flutter-create/idx-template.nix
@@ -1,13 +1,25 @@
-{pkgs, sample ? "none", template ? "app", blank ? false, platforms ? "web,android", ...}: {
-channel = "stable-25.05"; # or "unstable"
-    packages = [
-        pkgs.flutter
-    ];
-    bootstrap = ''
-        flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
-        mkdir "$out"/.idx
-        cp ${./dev.nix} "$out"/.idx/dev.nix
-        install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
-        chmod -R u+w "$out"
-    '';
+{ pkgs, sample ? "none", template ? "app", blank ? false, platforms ? "web,android", ... }: {
+  channel = "stable-25.05";
+
+  packages = [
+    pkgs.curl
+    pkgs.gnutar
+    pkgs.xz
+    pkgs.git
+    pkgs.busybox
+    pkgs.flutter
+  ];
+
+  bootstrap = ''
+    flutter create "$out" \
+      --template="${template}" \
+      --platforms="${platforms}" \
+      ${if sample == "none" then "" else "--sample=${sample}"} \
+      ${if blank then "-e" else ""}
+
+    mkdir "$out"/.idx
+    cp ${./dev.nix} "$out"/.idx/dev.nix
+    install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
+    chmod -R u+w "$out"
+  '';
 }
